90a89a47309ab61ef6ccd63ae2acd99e3b33e272
x1alfa <41350153+x1alfa@users.noreply.github.com>
2022-11-25 19:01:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
-ssh-honeypot -r /ssh-honeypot/ssh-honeypot.rsa -p 22 -u nobody
+ssh-honeypot -r /ssh-honeypot/ssh-honeypot.rsa -p 22 -u nobody -l /log/ssh-honeypot.log
 echo "SSH Honeypot is Running..."
 exec "$@"
